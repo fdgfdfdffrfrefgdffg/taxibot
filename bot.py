aaa99d25dfe50f20480bbb25527cb21e50f04bfb
fdgfdfdffrfrefgdffg <xudoyberganovodilbek6@gmail.com>
2024-11-05 16:31:43 +0500

    commit

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 from aiogram import Bot, Dispatcher
+from aiogram.client.session.aiohttp import AiohttpSession
 from config import BOT_TOKEN
 import message, callback
 from sqldata import close_db
+
 import logging
 from asyncio import run
 dp = Dispatcher()
@@ -12,7 +14,10 @@ dp = Dispatcher()
 
 
 async def main():
-    bot = Bot(BOT_TOKEN)
+    
+    session = AiohttpSession(proxy="http://proxy.server:3128/")
+    bot = Bot(BOT_TOKEN, session=session)
+
     logging.basicConfig(level=logging.INFO)
     dp.include_router(message.router)
     dp.include_router(callback.router)
