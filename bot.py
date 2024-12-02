5f5da4adb364394f34528cf80c181fc4b9bcf57c
fdgfdfdffrfrefgdffg <xudoyberganovodilbek6@gmail.com>
2024-12-02 14:19:34 +0500

    commit

--- bot.py
+++ bot.py
@@ -59,8 +59,8 @@ async def main():
     # bot = Bot(BOT_TOKEN)
     logging.basicConfig(level=logging.INFO)
 
-    # dp.include_router(message.router)
-    # dp.include_router(callback.router)
+    dp.include_router(message.router)
+    dp.include_router(callback.router)
     await dp.start_polling(bot)
     close_db()
 run(main())
